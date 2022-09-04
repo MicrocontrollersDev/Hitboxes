b9aa598d6ea4a07f6aa513d216f877da2180e48f
Nox <96212795+Noxiuam@users.noreply.github.com>
2022-09-04 12:14:45 -0400

    Finish Hitboxes mod

--- src/main/java/gq/noxiuam/hitboxes/config/HitBoxesConfig.java
+++ src/main/java/gq/noxiuam/hitboxes/config/HitBoxesConfig.java
@@ -7,299 +7,149 @@ import gq.noxiuam.hitboxes.HitBoxes;
 import cc.polyfrost.oneconfig.config.annotations.Switch;
 import cc.polyfrost.oneconfig.config.data.Mod;
 import cc.polyfrost.oneconfig.config.data.ModType;
+import net.minecraft.client.entity.AbstractClientPlayer;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.item.EntityItem;
+import net.minecraft.entity.item.EntityXPOrb;
+import net.minecraft.entity.projectile.EntityThrowable;
 
 public class HitBoxesConfig extends cc.polyfrost.oneconfig.config.Config {
 
     // player
-    @Switch(
-            name = "Player Hitbox",
-            subcategory = "Player Hitbox Options"
-    )
-    public static boolean showPlayerHitbox = true;
+    @Switch(name = "Player Hitbox", subcategory = "Player Hitbox Options")
+    public boolean showPlayerHitbox = true;
 
-    @Color(
-            name = "Player Hitbox Color",
-            subcategory = "Player Hitbox Options"
-    )
-    public static OneColor playerHitBoxColor = new OneColor(-1);
+    @Checkbox(name = "Dashed", subcategory = "Player Hitbox Options")
+    public boolean dashedPlayerHitbox = false;
 
-    @Checkbox(
-            name = "Dashed",
-            subcategory = "Player Hitbox Options",
-            size = 2
-    )
-    public static boolean dashedPlayerHitbox = false;
+    @Switch(name = "Player Hitbox Outline", subcategory = "Player Hitbox Options")
+    public boolean showPlayerOutline = true;
 
-    @Switch(
-            name = "Player Hitbox Outline",
-            subcategory = "Player Hitbox Options"
-    )
-    public static boolean showPlayerOutline = true;
+    @Color(name = "Player Hitbox Outline Color", subcategory = "Player Hitbox Options")
+    public OneColor playerOutlineColor = new OneColor(-1);
 
-    @Color(
-            name = "Player Hitbox Outline Color",
-            subcategory = "Player Hitbox Options"
-    )
-    public static OneColor playerOutlineColor = new OneColor(-1);
+    @Switch(name = "Player Eye Height", subcategory = "Player Hitbox Options")
+    public boolean showPlayerEyeHeight = true;
 
-    @Switch(
-            name = "Player Eye Height",
-            subcategory = "Player Hitbox Options"
-    )
-    public static boolean showPlayerEyeHeight = true;
+    @Color(name = "Player Eye Height Color", subcategory = "Player Hitbox Options")
+    public OneColor playerEyeHeightColor = new OneColor(0xFFFF0000);
 
-    @Color(
-            name = "Player Eye Height Color",
-            subcategory = "Player Hitbox Options"
-    )
-    public static OneColor playerEyeHeightColor = new OneColor(0xFFFF0000);
+    @Switch(name = "Player Look Vector", subcategory = "Player Hitbox Options")
+    public boolean showPlayerLookVector = true;
 
-    @Switch(
-            name = "Player Look Vector",
-            subcategory = "Player Hitbox Options"
-    )
-    public static boolean showPlayerLookVector = true;
-
-    @Color(
-            name = "Player Look Vector Color",
-            subcategory = "Player Hitbox Options"
-    )
-    public static OneColor playerLookVectorColor = new OneColor(0xFF0000FF);
+    @Color(name = "Player Look Vector Color", subcategory = "Player Hitbox Options")
+    public OneColor playerLookVectorColor = new OneColor(0xFF0000FF);
 
     // mob
-    @Switch(
-            name = "Mob Hitbox",
-            subcategory = "Mob Hitbox Options"
-    )
-    public static boolean showMobHitbox = true;
+    @Switch(name = "Mob Hitbox", subcategory = "Mob Hitbox Options")
+    public boolean showMobHitbox = true;
 
-    @Color(
-            name = "Mob Hitbox Color",
-            subcategory = "Mob Hitbox Options"
-    )
-    public static OneColor mobHitBoxColor = new OneColor(-1);
+    @Checkbox(name = "Dashed", subcategory = "Mob Hitbox Options")
+    public boolean dashedMobHitbox = false;
 
-    @Checkbox(
-            name = "Dashed",
-            subcategory = "Mob Hitbox Options",
-            size = 2
-    )
-    public static boolean dashedMobHitbox = false;
+    @Switch(name = "Mob Hitbox Outline", subcategory = "Mob Hitbox Options")
+    public boolean showMobOutline = true;
 
-    @Switch(
-            name = "Mob Hitbox Outline",
-            subcategory = "Mob Hitbox Options"
-    )
-    public static boolean showMobOutline = true;
+    @Color(name = "Mob Outline Color", subcategory = "Mob Hitbox Options")
+    public OneColor mobOutlineColor = new OneColor(-1);
 
-    @Color(
-            name = "Mob Outline Color",
-            subcategory = "Mob Hitbox Options"
-    )
-    public static OneColor mobOutlineColor = new OneColor(-1);
+    @Switch(name = "Mob Eye Height", subcategory = "Mob Hitbox Options")
+    public boolean showMobEyeHeight = true;
 
-    @Switch(
-            name = "Mob Eye Height",
-            subcategory = "Mob Hitbox Options"
-    )
-    public static boolean showMobEyeHeight = true;
+    @Color(name = "Mob Eye Height Color", subcategory = "Mob Hitbox Options")
+    public OneColor mobEyeHeightColor = new OneColor(0xFFFF0000);
 
-    @Color(
-            name = "Mob Eye Height Color",
-            subcategory = "Mob Hitbox Options"
-    )
-    public static OneColor mobEyeHeightColor = new OneColor(0xFFFF0000);
+    @Switch(name = "Mob Look Vector", subcategory = "Mob Hitbox Options")
+    public boolean showMobLookVector = true;
 
-    @Switch(
-            name = "Mob Look Vector",
-            subcategory = "Mob Hitbox Options"
-    )
-    public static boolean showMobLookVector = true;
-
-    @Color(
-            name = "Mob Look Vector Color",
-            subcategory = "Mob Hitbox Options"
-    )
-    public static OneColor mobLookVectorColor = new OneColor(0xFF0000FF);
+    @Color(name = "Mob Look Vector Color", subcategory = "Mob Hitbox Options")
+    public OneColor mobLookVectorColor = new OneColor(0xFF0000FF);
 
     // item
-    @Switch(
-            name = "Item Hitbox",
-            subcategory = "Item Hitbox Options"
-    )
-    public static boolean showItemHitbox = true;
+    @Switch(name = "Item Hitbox", subcategory = "Item Hitbox Options")
+    public boolean showItemHitbox = false;
 
-    @Color(
-            name = "Item Hitbox Color",
-            subcategory = "Item Hitbox Options"
-    )
-    public static OneColor itemHitBoxColor = new OneColor(-1);
-    
-    @Checkbox(
-            name = "Dashed",
-            subcategory = "Item Hitbox Options",
-            size = 2
-    )
-    public static boolean dashedItemHitbox = false;
-    
-    @Switch(
-            name = "Item Hitbox Outline",
-            subcategory = "Item Hitbox Options"
-    )
-    public static boolean showItemOutline = true;
+    @Checkbox(name = "Dashed", subcategory = "Item Hitbox Options")
+    public boolean dashedItemHitbox = false;
 
-    @Color(
-            name = "Item Outline Color",
-            subcategory = "Item Hitbox Options"
-    )
-    public static OneColor itemOutlineColor = new OneColor(-1);
+    @Switch(name = "Item Hitbox Outline", subcategory = "Item Hitbox Options")
+    public boolean showItemOutline = true;
 
-    @Switch(
-            name = "Item Eye Height",
-            subcategory = "Item Hitbox Options"
-    )
-    public static boolean showItemEyeHeight = true;
+    @Color(name = "Item Outline Color", subcategory = "Item Hitbox Options")
+    public OneColor itemOutlineColor = new OneColor(-1);
 
-    @Color(
-            name = "Item Eye Height Color",
-            subcategory = "Item Hitbox Options"
-    )
-    public static OneColor itemEyeHeightColor = new OneColor(0xFFFF0000);
+    @Switch(name = "Item Eye Height", subcategory = "Item Hitbox Options")
+    public boolean showItemEyeHeight = true;
 
-    @Switch(
-            name = "Item Look Vector",
-            subcategory = "Item Hitbox Options"
-    )
-    public static boolean showItemLookVector = true;
+    @Color(name = "Item Eye Height Color", subcategory = "Item Hitbox Options")
+    public OneColor itemEyeHeightColor = new OneColor(0xFFFF0000);
 
-    @Color(
-            name = "Item Look Vector Color",
-            subcategory = "Item Hitbox Options"
-    )
-    public static OneColor itemLookVectorColor = new OneColor(0xFF0000FF);
+    @Switch(name = "Item Look Vector", subcategory = "Item Hitbox Options")
+    public boolean showItemLookVector = true;
+
+    @Color(name = "Item Look Vector Color", subcategory = "Item Hitbox Options")
+    public OneColor itemLookVectorColor = new OneColor(0xFF0000FF);
 
     // projectiles
-    @Switch(
-            name = "Projectile Hitbox",
-            subcategory = "Projectile Hitbox Options"
-    )
-    public static boolean showProjectileHitbox = true;
+    @Switch(name = "Projectile Hitbox", subcategory = "Projectile Hitbox Options")
+    public boolean showProjectileHitbox = false;
 
-    @Color(
-            name = "Projectile Hitbox Color",
-            subcategory = "Projectile Hitbox Options"
-    )
-    public static OneColor projectileHitBoxColor = new OneColor(-1);
+    @Checkbox(name = "Dashed", subcategory = "Projectile Hitbox Options")
+    public boolean dashedProjectileHitbox = false;
 
-    @Checkbox(
-            name = "Dashed",
-            subcategory = "Projectile Hitbox Options",
-            size = 2
-    )
-    public static boolean dashedProjectileHitbox = false;
-    
-    @Switch(
-            name = "Projectile Hitbox Outline",
-            subcategory = "Projectile Hitbox Options"
-    )
-    public static boolean showProjectileOutline = true;
+    @Switch(name = "Projectile Hitbox Outline", subcategory = "Projectile Hitbox Options")
+    public boolean showProjectileOutline = true;
 
-    @Color(
-            name = "Projectile Outline Color",
-            subcategory = "Projectile Hitbox Options"
-    )
-    public static OneColor projectileOutlineColor = new OneColor(-1);
+    @Color(name = "Projectile Outline Color", subcategory = "Projectile Hitbox Options")
+    public OneColor projectileOutlineColor = new OneColor(-1);
 
-    @Switch(
-            name = "Projectile Eye Height",
-            subcategory = "Projectile Hitbox Options"
-    )
-    public static boolean showProjectileEyeHeight = true;
+    @Switch(name = "Projectile Eye Height", subcategory = "Projectile Hitbox Options")
+    public boolean showProjectileEyeHeight = true;
 
-    @Color(
-            name = "Projectile Eye Height Color",
-            subcategory = "Projectile Hitbox Options"
-    )
-    public static OneColor projectileEyeHeightColor = new OneColor(0xFFFF0000);
+    @Color(name = "Projectile Eye Height Color", subcategory = "Projectile Hitbox Options")
+    public OneColor projectileEyeHeightColor = new OneColor(0xFFFF0000);
 
-    @Switch(
-            name = "Projectile Look Vector",
-            subcategory = "Projectile Hitbox Options"
-    )
-    public static boolean showProjectileLookVector = true;
+    @Switch(name = "Projectile Look Vector", subcategory = "Projectile Hitbox Options")
+    public boolean showProjectileLookVector = true;
 
-    @Color(
-            name = "Projectile Look Vector Color",
-            subcategory = "Projectile Hitbox Options"
-    )
-    public static OneColor projectileLookVectorColor = new OneColor(0xFF0000FF);
+    @Color(name = "Projectile Look Vector Color", subcategory = "Projectile Hitbox Options")
+    public OneColor projectileLookVectorColor = new OneColor(0xFF0000FF);
 
     // xp orb
-    @Switch(
-            name = "XP Orb Hitbox",
-            subcategory = "XP Orb Hitbox Options"
-    )
-    public static boolean showXPOrbHitbox = true;
+    @Switch(name = "XP Orb Hitbox", subcategory = "XP Orb Hitbox Options")
+    public boolean showXPOrbHitbox = false;
 
-    @Color(
-            name = "XP Orb Hitbox Color",
-            subcategory = "XP Orb Hitbox Options"
-    )
-    public static OneColor xpOrbHitBoxColor = new OneColor(-1);
+    @Checkbox(name = "Dashed", subcategory = "XP Orb Hitbox Options")
+    public boolean dashedXPOrbHitbox = false;
 
-    @Checkbox(
-            name = "Dashed",
-            subcategory = "XP Orb Hitbox Options",
-            size = 2
-    )
-    public static boolean dashedXPOrbHitbox = false;
+    @Switch(name = "XP Orb Hitbox Outline", subcategory = "XP Orb Hitbox Options")
+    public boolean showXPOrbOutline = true;
 
-    @Switch(
-            name = "XP Orb Hitbox Outline",
-            subcategory = "XP Orb Hitbox Options"
-    )
-    public static boolean showXPOrbOutline = true;
+    @Color(name = "XP Orb Outline Color", subcategory = "XP Orb Hitbox Options")
+    public OneColor xpOrbOutlineColor = new OneColor(-1);
 
-    @Color(
-            name = "XP Orb Outline Color",
-            subcategory = "XP Orb Hitbox Options"
-    )
-    public static OneColor xpOrbOutlineColor = new OneColor(-1);
+    @Switch(name = "XP Orb Eye Height", subcategory = "XP Orb Hitbox Options")
+    public boolean showXPOrbEyeHeight = true;
 
-    @Switch(
-            name = "XP Orb Eye Height",
-            subcategory = "XP Orb Hitbox Options"
-    )
-    public static boolean showXPOrbEyeHeight = true;
+    @Color(name = "XP Orb Eye Height Color", subcategory = "XP Orb Hitbox Options")
+    public OneColor xpOrbEyeHeightColor = new OneColor(0xFFFF0000);
 
-    @Color(
-            name = "XP Orb Eye Height Color",
-            subcategory = "XP Orb Hitbox Options"
-    )
-    public static OneColor xpOrbEyeHeightColor = new OneColor(0xFFFF0000);
+    @Switch(name = "XP Orb Look Vector", subcategory = "XP Orb Hitbox Options")
+    public boolean showXPOrbLookVector = true;
 
-    @Switch(
-            name = "XP Orb Look Vector",
-            subcategory = "XP Orb Hitbox Options"
-    )
-    public static boolean showXPOrbLookVector = true;
-
-    @Color(
-            name = "XP Orb Look Vector Color",
-            subcategory = "XP Orb Hitbox Options"
-    )
-    public static OneColor xpOrbLookVectorColor = new OneColor(0xFF0000FF);
+    @Color(name = "XP Orb Look Vector Color", subcategory = "XP Orb Hitbox Options")
+    public OneColor xpOrbLookVectorColor = new OneColor(0xFF0000FF);
 
     public HitBoxesConfig() {
         super(new Mod(HitBoxes.NAME, ModType.UTIL_QOL), HitBoxes.MODID + ".json");
         initialize();
+
         // player
         addDependency("showPlayerOutline", () -> showPlayerHitbox);
         addDependency("dashedPlayerHitbox", () -> showPlayerHitbox);
         addDependency("showPlayerEyeHeight", () -> showPlayerHitbox);
         addDependency("showPlayerLookVector", () -> showPlayerHitbox);
 
-        addDependency("playerHitBoxColor", () -> showPlayerHitbox);
         addDependency("playerOutlineColor", () -> showPlayerHitbox && showPlayerOutline);
         addDependency("playerEyeHeightColor", () -> showPlayerHitbox && showPlayerEyeHeight);
         addDependency("playerLookVectorColor", () -> showPlayerHitbox && showPlayerLookVector);
@@ -310,7 +160,6 @@ public class HitBoxesConfig extends cc.polyfrost.oneconfig.config.Config {
         addDependency("showMobEyeHeight", () -> showMobHitbox);
         addDependency("showMobLookVector", () -> showMobHitbox);
 
-        addDependency("mobHitBoxColor", () -> showMobHitbox);
         addDependency("mobOutlineColor", () -> showMobHitbox && showMobOutline);
         addDependency("mobEyeHeightColor", () -> showMobHitbox && showMobEyeHeight);
         addDependency("mobLookVectorColor", () -> showMobHitbox && showMobLookVector);
@@ -321,7 +170,6 @@ public class HitBoxesConfig extends cc.polyfrost.oneconfig.config.Config {
         addDependency("showItemEyeHeight", () -> showItemHitbox);
         addDependency("showItemLookVector", () -> showItemHitbox);
 
-        addDependency("itemHitBoxColor", () -> showItemHitbox);
         addDependency("itemOutlineColor", () -> showItemHitbox && showItemOutline);
         addDependency("itemEyeHeightColor", () -> showItemHitbox && showItemEyeHeight);
         addDependency("itemLookVectorColor", () -> showItemHitbox && showItemLookVector);
@@ -332,7 +180,6 @@ public class HitBoxesConfig extends cc.polyfrost.oneconfig.config.Config {
         addDependency("showProjectileEyeHeight", () -> showProjectileHitbox);
         addDependency("showProjectileLookVector", () -> showProjectileHitbox);
 
-        addDependency("projectileHitBoxColor", () -> showProjectileHitbox);
         addDependency("projectileOutlineColor", () -> showProjectileHitbox && showProjectileOutline);
         addDependency("projectileEyeHeightColor", () -> showProjectileHitbox && showProjectileEyeHeight);
         addDependency("projectileLookVectorColor", () -> showProjectileHitbox && showProjectileLookVector);
@@ -343,10 +190,30 @@ public class HitBoxesConfig extends cc.polyfrost.oneconfig.config.Config {
         addDependency("showXPOrbEyeHeight", () -> showXPOrbHitbox);
         addDependency("showXPOrbLookVector", () -> showXPOrbHitbox);
 
-        addDependency("xpOrbHitBoxColor", () -> showXPOrbHitbox);
         addDependency("xpOrbOutlineColor", () -> showXPOrbHitbox && showXPOrbOutline);
         addDependency("xpOrbEyeHeightColor", () -> showXPOrbHitbox && showXPOrbEyeHeight);
         addDependency("xpOrbLookVectorColor", () -> showXPOrbHitbox && showXPOrbLookVector);
     }
+
+    /**
+     * Used to get the options in the form of an array.
+     *
+     * @param entityIn - The entity that will be given a very special hitbox.
+     */
+    public boolean[] getEntityBooleanOptions(Entity entityIn) {
+
+        if (entityIn instanceof AbstractClientPlayer) {
+            return new boolean[]{ showPlayerOutline, showPlayerEyeHeight, showPlayerLookVector };
+        } else if (entityIn instanceof EntityItem) {
+            return new boolean[]{ showPlayerOutline, showPlayerEyeHeight, showPlayerLookVector };
+        } else if (entityIn instanceof EntityThrowable) {
+            return new boolean[]{ showProjectileOutline, showProjectileEyeHeight, showProjectileLookVector };
+        } else if (entityIn instanceof EntityXPOrb) {
+            return new boolean[]{ showXPOrbOutline, showXPOrbEyeHeight, showXPOrbLookVector };
+        }
+
+        return new boolean[]{ showMobOutline, showMobEyeHeight, showMobLookVector };
+    }
+
 }
 
--- src/main/java/gq/noxiuam/hitboxes/mixin/RenderManagerMixin.java
+++ src/main/java/gq/noxiuam/hitboxes/mixin/RenderManagerMixin.java
@@ -1,5 +1,6 @@
 package gq.noxiuam.hitboxes.mixin;
 
+import cc.polyfrost.oneconfig.config.core.OneColor;
 import gq.noxiuam.hitboxes.HitBoxes;
 import gq.noxiuam.hitboxes.config.HitBoxesConfig;
 import net.minecraft.client.entity.AbstractClientPlayer;
@@ -11,6 +12,10 @@ import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.item.EntityItem;
+import net.minecraft.entity.item.EntityXPOrb;
+import net.minecraft.entity.projectile.EntityArrow;
+import net.minecraft.entity.projectile.EntityThrowable;
 import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.util.Vec3;
 import org.lwjgl.opengl.GL11;
@@ -27,46 +32,36 @@ import static org.lwjgl.opengl.GL11.GL_LINE_STIPPLE;
 public class RenderManagerMixin {
 
     @Inject(method = "doRenderEntity", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/entity/Render;doRender(Lnet/minecraft/entity/Entity;DDDFF)V"))
-    public void redirect(Entity entityIn, double x, double y, double z, float entityYaw, float partialTicks, boolean p_147939_10_, CallbackInfoReturnable<Boolean> cir) {
+    public void overrideHitboxRendering(Entity entityIn, double x, double y, double z, float entityYaw, float partialTicks, boolean p_147939_10_, CallbackInfoReturnable<Boolean> cir) {
 
-        if (HitBoxesConfig.showPlayerHitbox && entityIn instanceof AbstractClientPlayer) {
-            GlStateManager.depthMask(false);
-            GlStateManager.disableTexture2D();
-            GlStateManager.disableLighting();
-            GlStateManager.disableCull();
-            GlStateManager.disableBlend();
-            float f = entityIn.width / 2.0f;
-            AxisAlignedBB axisalignedbb = entityIn.getEntityBoundingBox();
+        HitBoxesConfig config = HitBoxes.INSTANCE.config;
 
-            if (HitBoxesConfig.dashedPlayerHitbox) {
-                GL11.glPushAttrib(8192);
-                GL11.glLineStipple(5, (short) 0xAAAA);
-                GL11.glEnable(GL_LINE_STIPPLE);
-                GL11.glBegin(GL_LINES);
-                GL11.glEnd();
-            }
+        boolean showPlayerHitbox = config.showPlayerHitbox && entityIn instanceof AbstractClientPlayer;
+        boolean showMobHitbox = config.showMobHitbox
+                && (!(entityIn instanceof AbstractClientPlayer)
+                && !(entityIn instanceof EntityItem)
+                && !(entityIn instanceof EntityThrowable)
+                && !(entityIn instanceof EntityXPOrb)
+        );
+        boolean showItemHitbox = config.showItemHitbox && entityIn instanceof EntityItem;
+        boolean showProjectileHitbox = config.showProjectileHitbox && (entityIn instanceof EntityThrowable || entityIn instanceof EntityArrow);
+        boolean showXPOrbHitbox = config.showXPOrbHitbox && entityIn instanceof EntityXPOrb;
 
-            AxisAlignedBB axisalignedbb1 = new AxisAlignedBB(axisalignedbb.minX - entityIn.posX + x, axisalignedbb.minY - entityIn.posY + y, axisalignedbb.minZ - entityIn.posZ + z, axisalignedbb.maxX - entityIn.posX + x, axisalignedbb.maxY - entityIn.posY + y, axisalignedbb.maxZ - entityIn.posZ + z);
-            RenderGlobal.drawOutlinedBoundingBox(axisalignedbb1, 255, 255, 255, 255);
-            RenderGlobal.drawOutlinedBoundingBox(new AxisAlignedBB(x - (double)f, y + (double)entityIn.getEyeHeight() - (double)0.01f, z - (double)f, x + (double)f, y + (double)entityIn.getEyeHeight() + (double)0.01f, z + (double)f), 255, 0, 0, 255);
-
-            if (HitBoxesConfig.dashedPlayerHitbox) {
-                GL11.glPopAttrib();
-            }
-
-            Tessellator tessellator = Tessellator.getInstance();
-            WorldRenderer worldrenderer = tessellator.getWorldRenderer();
-            Vec3 vec3 = entityIn.getLook(partialTicks);
-            worldrenderer.begin(3, DefaultVertexFormats.POSITION_COLOR);
-            worldrenderer.pos(x, y + (double)entityIn.getEyeHeight(), z).color(0, 0, 255, 255).endVertex();
-            worldrenderer.pos(x + vec3.xCoord * 2.0, y + (double)entityIn.getEyeHeight() + vec3.yCoord * 2.0, z + vec3.zCoord * 2.0).color(0, 0, 255, 255).endVertex();
-            tessellator.draw();
-
-            GlStateManager.enableTexture2D();
-            GlStateManager.enableLighting();
-            GlStateManager.enableCull();
-            GlStateManager.disableBlend();
-            GlStateManager.depthMask(true);
+        if (showPlayerHitbox) {
+            renderHitbox(entityIn, x, y, z, partialTicks,
+                    config.dashedPlayerHitbox, config.playerOutlineColor, config.playerEyeHeightColor, config.playerLookVectorColor);
+        } else if (showMobHitbox) {
+            renderHitbox(entityIn, x, y, z, partialTicks,
+                    config.dashedMobHitbox, config.mobOutlineColor, config.mobEyeHeightColor, config.mobLookVectorColor);
+        } else if (showItemHitbox) {
+            renderHitbox(entityIn, x, y, z, partialTicks,
+                    config.dashedItemHitbox, config.itemOutlineColor, config.itemEyeHeightColor, config.itemLookVectorColor);
+        } else if (showProjectileHitbox) {
+            renderHitbox(entityIn, x, y, z, partialTicks,
+                    config.dashedProjectileHitbox, config.projectileOutlineColor, config.projectileEyeHeightColor, config.projectileLookVectorColor);
+        } else if (showXPOrbHitbox) {
+            renderHitbox(entityIn, x, y, z, partialTicks,
+                    config.dashedXPOrbHitbox, config.xpOrbOutlineColor, config.xpOrbEyeHeightColor, config.xpOrbLookVectorColor);
         }
 
     }
@@ -74,6 +69,62 @@ public class RenderManagerMixin {
     @Inject(method = "renderDebugBoundingBox", at = @At(value = "HEAD"), cancellable = true)
     public void cancelVanillaHitboxes(CallbackInfo ci) {
         ci.cancel();
+    }
+
+    private void renderHitbox(Entity entityIn, double x, double y, double z, float partialTicks, boolean dashed, OneColor outlineColor, OneColor eyeHeightColor, OneColor lookVectorColor) {
+
+        boolean[] options = HitBoxes.INSTANCE.config.getEntityBooleanOptions(entityIn);
+
+        boolean showOutline = options[0];
+        boolean showEyeHeight = options[1];
+        boolean showLookVector = options[2];
+
+        GlStateManager.depthMask(false);
+        GlStateManager.disableTexture2D();
+        GlStateManager.disableLighting();
+        GlStateManager.disableCull();
+        GlStateManager.disableBlend();
+        float f = entityIn.width / 2.0f;
+        AxisAlignedBB axisalignedbb = entityIn.getEntityBoundingBox();
+
+        if (dashed) {
+            GL11.glPushAttrib(8192);
+            GL11.glLineStipple(5, (short) 0xAAAA);
+            GL11.glEnable(GL_LINE_STIPPLE);
+            GL11.glBegin(GL_LINES);
+            GL11.glEnd();
+        }
+
+        if (showOutline) {
+            AxisAlignedBB axisalignedbb1 = new AxisAlignedBB(axisalignedbb.minX - entityIn.posX + x, axisalignedbb.minY - entityIn.posY + y, axisalignedbb.minZ - entityIn.posZ + z, axisalignedbb.maxX - entityIn.posX + x, axisalignedbb.maxY - entityIn.posY + y, axisalignedbb.maxZ - entityIn.posZ + z);
+            RenderGlobal.drawOutlinedBoundingBox(axisalignedbb1, outlineColor.getRed(), outlineColor.getGreen(), outlineColor.getBlue(), outlineColor.getAlpha());
+        }
+
+        if (showEyeHeight) {
+            RenderGlobal.drawOutlinedBoundingBox(new AxisAlignedBB(x - (double)f, y + (double)entityIn.getEyeHeight() - (double)0.01f, z - (double)f, x + (double)f, y + (double)entityIn.getEyeHeight() + (double)0.01f, z + (double)f),
+                    eyeHeightColor.getRed(), eyeHeightColor.getGreen(), eyeHeightColor.getBlue(), eyeHeightColor.getAlpha());
+        }
+
+        if (dashed) {
+            GL11.glPopAttrib();
+        }
+
+        if (showLookVector) {
+            Tessellator tessellator = Tessellator.getInstance();
+            WorldRenderer worldrenderer = tessellator.getWorldRenderer();
+            Vec3 vec3 = entityIn.getLook(partialTicks);
+            worldrenderer.begin(3, DefaultVertexFormats.POSITION_COLOR);
+            worldrenderer.pos(x, y + (double)entityIn.getEyeHeight(), z).color(lookVectorColor.getRed(), lookVectorColor.getGreen(), lookVectorColor.getBlue(), lookVectorColor.getAlpha()).endVertex();
+            worldrenderer.pos(x + vec3.xCoord * 2.0, y + (double)entityIn.getEyeHeight() + vec3.yCoord * 2.0, z + vec3.zCoord * 2.0).color(0, 0, 255, 255).endVertex();
+            tessellator.draw();
+        }
+
+        GlStateManager.enableTexture2D();
+        GlStateManager.enableLighting();
+        GlStateManager.enableCull();
+        GlStateManager.disableBlend();
+        GlStateManager.depthMask(true);
+
     }
 
 }
